f4a3c26d368b3d6ef56d08affd58a6ce68e24964
Ross Hamilton <rosshamilton.dev@gmail.com>
2016-10-06 21:11:30 +0100

    rna_transcription 1st complete

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,8 +1,20 @@
 class Complement
 
-  RNA_complement_logic = { 'C' => 'G', 'G' => 'C', 'T' => 'A', 'A' => 'U' }
+  RNA_COMPLEMENTS = { 'C' => 'G', 'G' => 'C', 'T' => 'A', 'A' => 'U' }
 
   def self.of_dna(nucleotide)
-    nucleotide.gsub(/[CGTA]/, RNA_complement_logic )
+    is_valid_dna?(nucleotide) ? convert_dna_to_rna(nucleotide) : ""
   end
+
+  def self.is_valid_dna?(nucleotide)
+    nucleotide.split("").all? { |char| RNA_COMPLEMENTS.keys.include? char }
+  end
+
+  def self.convert_dna_to_rna(nucleotide)
+    nucleotide.gsub(/[CGTA]/, RNA_COMPLEMENTS)
+  end
+end
+
+module BookKeeping
+  VERSION = 4
 end
