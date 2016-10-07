5f7469fd593f99d122d0fe78688eeccdc0f3c5f4
Ross Hamilton <rosshamilton.dev@gmail.com>
2016-10-07 21:59:53 +0100

    refactored

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -4,18 +4,18 @@ class Raindrops
 
     return_product = ""
 
-    return_product += "Pling" if divisors_of(number).include?(3)
-    return_product += "Plang" if divisors_of(number).include?(5)
-    return_product += "Plong" if divisors_of(number).include?(7)
+    return_product += "Pling" if divisors_of_include?(number, 3)
+    return_product += "Plang" if divisors_of_include?(number, 5)
+    return_product += "Plong" if divisors_of_include?(number, 7)
 
     return_product == "" ? number.to_s : return_product
   end
 
-  def self.divisors_of(number)
-    (1..number).select { |n| number % n == 0}
+  def self.divisors_of_include?(number, factor)
+    (1..number).select { |n| number % n == 0}.include?(factor)
   end
 end
 
 module BookKeeping
-  VERSION = 4
+  VERSION = 3
 end
