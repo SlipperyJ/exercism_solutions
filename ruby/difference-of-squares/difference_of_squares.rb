173730294f88ee1589a2c0592090a4ddf1c73ef7
Ross Hamilton <rosshamilton.dev@gmail.com>
2016-10-13 23:58:04 +0100

    refactor

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -1,9 +1,18 @@
 class Squares
   def initialize(number)
     @number = number
+    @number_range = number_range_to_array(number)
   end
 
   def square_of_sum
-    (1..@number).reduce(:+)**2
+    @number_range.reduce(:+)**2
+  end
+
+  def sum_of_squares
+    @number_range.map{|x| x**2}.reduce(:+)
+  end
+
+  def number_range_to_array(number)
+    (1..@number).to_a
   end
 end
